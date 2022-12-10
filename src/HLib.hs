ee92c304e22e74b1334de0dc7e5ecf435af3c02d
stla <laurent_step@yahoo.fr>
2022-12-11 00:06:07 +0100

    second commit

--- src/HLib.hs
+++ src/HLib.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 
 module HLib
     where
@@ -21,17 +22,42 @@ instance (AlgAdd.C a, Eq a) => AlgAdd.C (Spray a) where
     zero = HM.empty
     negate = negateSpray
 
-cleanSpray :: (AlgAdd.C a, Eq a) => Spray a -> Spray a
-cleanSpray p = HM.filter (/= AlgAdd.zero) p
+instance (AlgMod.C a a, Eq a) => AlgMod.C a (Spray a) where
+  lambda *> p = scaleSpray lambda p
 
-f :: AlgAdd.C a => Spray a -> Seq Int -> a -> Spray a
-f s powers coef = HM.insertWith (AlgAdd.+) powers coef s
+
+cleanSpray :: (AlgAdd.C a, Eq a) => Spray a -> Spray a
+cleanSpray = HM.filter (/= AlgAdd.zero)
 
 addSprays :: (AlgAdd.C a, Eq a) => Spray a -> Spray a -> Spray a
 addSprays p q = cleanSpray $ HM.foldlWithKey' f p q
+    where
+        f s powers coef = HM.insertWith (AlgAdd.+) powers coef s
 
 negateSpray :: AlgAdd.C a => Spray a -> Spray a
-negateSpray p = HM.map AlgAdd.negate p
+negateSpray = HM.map AlgAdd.negate 
+
+scaleSpray :: AlgMod.C a a => a -> Spray a -> Spray a
+scaleSpray lambda = HM.map (lambda AlgMod.*>)
+
+growSequence :: Seq Int -> Int -> Seq Int 
+growSequence s n = s >< t
+  where 
+    m = S.length s 
+    t = S.replicate (n - m) 0
+
+multMonomial :: AlgRing.C a => (Seq Int, a) -> (Seq Int, a) -> (Seq Int, a)
+multMonomial (pows1, coef1) (pows2, coef2) = 
+    (S.zipWith (+) pows1' pows2', coef1 AlgRing.* coef2) 
+  where
+    n1 = S.length pows1
+    n2 = S.length pows2
+    (pows1', pows2') = if n1 > n2 
+        then 
+            (pows1, growSequence pows2 n1)
+        else
+            (growSequence pows1 n2, pows2)
+
 
 p1 :: Spray Double
 p1 = HM.fromList [(S.fromList [1, 0], 2)]
