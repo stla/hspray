5bc2fa5973e500952d21af57ddc4feb4689cdcb2
stla <laurent_step@yahoo.fr>
2022-12-11 10:40:35 +0100

    Powers type

--- src/HLib.hs
+++ src/HLib.hs
@@ -11,11 +11,45 @@ import qualified Algebra.Ring     as AlgRing
 import           Data.Foldable    ( toList )
 import qualified Data.Sequence    as S
 import           Data.Sequence    ( Seq, (><), (|>) )
+import Data.Hashable
 import Data.HashMap.Strict (HashMap)
 import qualified Data.HashMap.Strict as HM
 
 
-type Spray a = HashMap (Seq Int) a
+data Powers = Powers 
+  {
+    exponents :: Seq Int,
+    nvariables :: Int
+  }
+  deriving Show
+
+growSequence :: Seq Int -> Int -> Int -> Seq Int 
+growSequence s m n = s >< t
+  where 
+    t = S.replicate (n - m) 0
+
+harmonize :: (Powers, Powers) -> (Powers, Powers)
+harmonize (pows1, pows2) = (Powers e1' n, Powers e2' n)
+  where
+    e1 = exponents pows1
+    e2 = exponents pows2
+    n1 = nvariables pows1
+    n2 = nvariables pows2
+    (e1', e2', n) = if n1 < n2
+      then
+        (growSequence e1 n1 n2, e2, n2)
+      else
+        (e1, growSequence e2 n2 n1, n1)
+
+instance Eq Powers where
+  pows1 == pows2 = (exponents pows1') == (exponents pows2')
+    where
+      (pows1', pows2') = harmonize (pows1, pows2)
+
+instance Hashable Powers where 
+  hashWithSalt k pows = hashWithSalt k (exponents pows, nvariables pows)
+
+type Spray a = HashMap Powers a
 
 instance (AlgAdd.C a, Eq a) => AlgAdd.C (Spray a) where
     p + q = addSprays p q
@@ -44,23 +78,13 @@ negateSpray = HM.map AlgAdd.negate
 scaleSpray :: AlgMod.C a a => a -> Spray a -> Spray a
 scaleSpray lambda = HM.map (lambda AlgMod.*>)
 
-growSequence :: Seq Int -> Int -> Seq Int 
-growSequence s n = s >< t
-  where 
-    m = S.length s 
-    t = S.replicate (n - m) 0
-
-multMonomial :: AlgRing.C a => (Seq Int, a) -> (Seq Int, a) -> (Seq Int, a)
+multMonomial :: AlgRing.C a => (Powers, a) -> (Powers, a) -> (Powers, a)
 multMonomial (pows1, coef1) (pows2, coef2) = 
-    (S.zipWith (+) pows1' pows2', coef1 AlgRing.* coef2) 
+    (pows, coef1 AlgRing.* coef2) 
   where
-    n1 = S.length pows1
-    n2 = S.length pows2
-    (pows1', pows2') = if n1 > n2 
-        then 
-            (pows1, growSequence pows2 n1)
-        else
-            (growSequence pows1 n2, pows2)
+    (pows1', pows2') = harmonize (pows1, pows2)
+    expts = S.zipWith (+) (exponents pows1') (exponents pows2')
+    pows = Powers expts (nvariables pows1') 
 
 multSprays :: (AlgRing.C a, Eq a) => Spray a -> Spray a -> Spray a
 multSprays p q = cleanSpray $ HM.fromListWith (AlgAdd.+) prods
@@ -75,15 +99,15 @@ lone n = HM.singleton pows AlgRing.one
   where
     pows = if n == 0 
       then 
-        S.empty 
+        Powers S.empty 0 
       else 
-        S.replicate (n - 1) AlgAdd.zero |> AlgRing.one
+        Powers (S.replicate (n - 1) AlgAdd.zero |> AlgRing.one) n
 
 p1 :: Spray Double
-p1 = HM.fromList [(S.fromList [1, 0], 2)]
+p1 = HM.fromList [(Powers (S.fromList [1, 0]) 2, 2)]
 
 p2 :: Spray Double
-p2 = HM.fromList [(S.fromList [1, 1], 3)]
+p2 = HM.fromList [(Powers (S.fromList [1, 1]) 2, 3)]
 
 p :: Spray Double
 p = p1 AlgRing.* p2
