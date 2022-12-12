62b7913b9ed41baa83c529e69c943f273159ac2b
stla <laurent_step@yahoo.fr>
2022-12-12 04:37:02 +0100

    unit test composeSpray

--- tests/Main.hs
+++ tests/Main.hs
@@ -10,7 +10,8 @@ import           Test.Tasty.HUnit               ( assertEqual
 
 main :: IO ()
 main = defaultMain $ testGroup
-  "Tests"
+  "Testing hspray"
+
   [ testCase "bombieriSpray" $ do
       let
         x = lone 1 :: Spray Rational
@@ -20,5 +21,20 @@ main = defaultMain $ testGroup
           (2 % 1) *^ ((2 % 1) *^ (x ^**^ 3 ^*^ y ^**^ 2)) ^+^ (4 % 1) *^ z ^+^ (5 % 1) *^ unitSpray
         bpoly =
           (24 % 1) *^ ((2 % 1) *^ (x ^**^ 3 ^*^ y ^**^ 2)) ^+^ (4 % 1) *^ z ^+^ (5 % 1) *^ unitSpray
-      assertEqual "" bpoly (bombieriSpray poly)
+      assertEqual "" bpoly (bombieriSpray poly),
+
+    testCase "composeSpray" $ do
+      let 
+        x = lone 1 :: Spray Int
+        y = lone 2 :: Spray Int
+        z = lone 3 :: Spray Int
+        p = 2 *^ (2 *^ (x ^**^ 3 ^*^ y ^**^ 2)) ^+^ 4 *^ z ^+^ 5 *^ unitSpray
+        px = x ^+^ y ^+^ z
+        py = x ^*^ y ^*^ z
+        pz = y ^**^ 2
+        q = composeSpray p [px, py, pz]
+        xyz = [2, 3, 4]
+        pxyz = map (\poly -> evalSpray poly xyz) [px, py, pz]
+      assertEqual "" (evalSpray p pxyz) (evalSpray q xyz)
+
   ]
