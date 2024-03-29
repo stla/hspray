d29808f5f35aa02ff8f78dc676df222690455522
stla <laurent_step@yahoo.fr>
2024-03-29 01:57:55 +0100

    gcdQXY

--- src/Math/Algebra/Hspray.hs
+++ src/Math/Algebra/Hspray.hs
@@ -1154,3 +1154,54 @@ gcdQX sprayA sprayB
           (degA'', ellA'') = degreeAndLeadingCoefficient sprayA''
           ellAq'' = getCoefficient [] ellA''
           delta = degA'' - degree sprayB''
+
+-- The GCD of two /bivariate/ sprays with rational coefficients
+gcdQXY :: Spray Rational -> Spray Rational -> Spray Rational
+gcdQXY sprayA sprayB
+  | n >= 3                        = error "gcdQXY: the sprays are not bivariate." 
+  | degree sprayB > degree sprayA = gcdQXY sprayB sprayA 
+  | sprayB == zeroSpray           = sprayA
+  | otherwise                     = go sprayA' sprayB' unitSpray unitSpray
+  where
+    n = max (numberOfVariables sprayA) (numberOfVariables sprayB)
+    content :: Spray Rational -> Spray Rational
+    content spray = foldl1 gcdQX coeffs'
+      where
+        coeffs   = sprayCoefficients spray
+        coeffs'  = map (swapVariables (1, 2)) coeffs
+    exactDivisionBy :: Spray Rational -> Spray Rational -> Spray Rational
+    exactDivisionBy b a = fst $ multivariateDivision a b 
+    reduceSpray :: Spray Rational -> Spray Rational
+    reduceSpray spray = foldl1 (^+^) $ zipWith (^*^) coeffs'' xmonoms
+      where
+        coeffs   = sprayCoefficients spray
+        coeffs'  = map (swapVariables (1, 2)) coeffs
+        cntnt    = foldl1 gcdQX coeffs'
+        coeffs'' = map (swapVariables (1,2) . (exactDivisionBy cntnt)) coeffs'
+        sprayX  = lone 1
+        deg     = length coeffs - 1
+        xmonoms = map (\k -> sprayX^**^k) [deg, deg-1 .. 0]
+    reduceSpray' :: Spray Rational -> Spray Rational -> Spray Rational
+    reduceSpray' spray divisor = foldl1 (^+^) $ zipWith (^*^) coeffs'' xmonoms
+      where
+        coeffs   = sprayCoefficients spray
+        coeffs'  = map (swapVariables (1, 2)) coeffs
+        -- divisor' = swapVariables (1, 2) divisor
+        coeffs'' = map (swapVariables (1,2) . (exactDivisionBy divisor)) coeffs'
+        sprayX  = lone 1
+        deg     = length coeffs - 1
+        xmonoms = map (\k -> sprayX^**^k) [deg, deg-1 .. 0]
+    a = content sprayA
+    b = content sprayB
+    d = gcdQX a b
+    sprayA' = reduceSpray' sprayA a
+    sprayB' = reduceSpray' sprayB b
+    go :: Spray Rational -> Spray Rational -> Spray Rational -> Spray Rational -> Spray Rational
+    go sprayA'' sprayB'' g h 
+      | sprayR == zeroSpray           = d ^*^ reduceSpray sprayB''
+      | numberOfVariables sprayR == 0 = d
+      | otherwise = go sprayB'' (reduceSpray' sprayR (g^*^ h^**^delta)) ellA'' (fst $ multivariateDivision (g^**^delta) (h^**^(delta-1)))
+        where
+          (_, (_, sprayR)) = pseudoDivision sprayA'' sprayB''
+          (degA'', ellA'') = degreeAndLeadingCoefficient sprayA''
+          delta = degA'' - degree sprayB''
